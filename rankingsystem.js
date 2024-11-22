03466907a4feb78035cb455bf0dd6b937ee79108
Aditya Sharma <adityasharma96655@gmail.com>
2024-11-22 14:17:00 +0530

    Add files via upload

---
+++ rankingsystem.js
@@ -0,0 +1,37 @@
+let marks;
+marks=100;
+
+    if(marks<40){
+        console.log("You got Fail");
+    }
+    else if(marks<=50){
+        console.log('You got grade C');
+    }
+    else if(marks<=60){
+        console.log('You got grade C+');
+    }
+    else if(marks<=65){
+        console.log('You got grade C++');
+    }
+    else if(marks<=70){
+        console.log('You got grade B');
+    }
+    else if(marks<=80){
+        console.log('You got grade B+');
+    }
+    else if(marks<=85){
+        console.log('You got grade B++');
+    }
+    else if(marks<=90){
+        console.log('You got grade A');
+    }
+    else if(marks<=95){
+        console.log('You got grade A+');
+    }
+    else if(marks<=100){
+        console.log('You got grade A++');
+    }
+    else{
+        console.log('Sorry you are fail');
+    }
+    
